3826d8e6224df48c3d35bf80e6eaaa38832da40b
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-11-30 22:29:45 +0100

    Pizza_mama_v1 gestion connexion au compte

--- C#/Projets_cs/pizza_mama_v1/pizza_mama_v1/Pages/Admin/Index.cshtml.cs
+++ C#/Projets_cs/pizza_mama_v1/pizza_mama_v1/Pages/Admin/Index.cshtml.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace pizza_mama_v1.Pages.Admin
 {
@@ -7,6 +13,21 @@ namespace pizza_mama_v1.Pages.Admin
     {
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync(string username, string password, string ReturnUrl)
+        {
+            if(username == "admin")
+            {
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, username)
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, "Login");
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                // Si authentification réussie on redirige vers Admin/Pizzas par défaut sinon vers l'url demandée
+                return Redirect(ReturnUrl == null ? "/Admin/Pizzas" : ReturnUrl);
+            }
+            return Page();
         }
     }
 }
