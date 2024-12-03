d6aeebac94fee24338c691be5ecf9a799c22ded3
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-12-03 21:35:27 +0100

    Pizza_mama_v1 Affichage pizzas et prix

--- C#/Projets_cs/pizza_mama_v1/pizza_mama_v1/Pages/MenuPizzas.cshtml.cs
+++ C#/Projets_cs/pizza_mama_v1/pizza_mama_v1/Pages/MenuPizzas.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pizza_mama_v1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace pizza_mama_v1.Pages
 {
     public class MenuPizzasModel : PageModel
     {
-        public void OnGet()
+        private readonly pizza_mama_v1.Data.DataContext _context;
+
+        public MenuPizzasModel(pizza_mama_v1.Data.DataContext context)
         {
+            _context = context;
+        }
+
+        public IList<Pizza> Pizza { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Pizza = await _context.Pizzas.ToListAsync();
         }
     }
 }
